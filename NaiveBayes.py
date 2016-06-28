79c526ae11d1683936523ad94fc46a4f28a0b28d
DannyGotTea <danielgallagher500@gmail.com>
2016-06-28 12:23:38 +0100

    frequency tables created with probability of each class

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -7,27 +7,67 @@ email_path = "/Users/dannyg/Desktop/Projects/NaiveBayesProject/NaiveBayesSpamFil
 email_files = [x for x in os.listdir(email_path)]
 
 emails = []
-labels = []
+labels = [] #0 = spam, 1 = not spam
 
-filter_chars = [',', '?', '  ']
+filter_chars = [',', '?', '!', '  ', '   ', '    ']
 
 for e in email_files:
 	with open(email_path + e, 'r') as email:
-		text = [x for x in email.read().translate(None, '\n').split(' ')]
+		text = [x for x in email.read().replace('\n', ' ')]
 
 		for ch in filter_chars:
 			text = [x.replace(ch, ' ') for x in text]
 
+		text = ''.join(text).split()
+
 		lbl = text[-1]
-
 		labels.append(lbl)
-
 		del text[-1] #so it doesn't try use the label in predctions
 
-		emails.append(text)
+		emails.append(text) 
 
-print emails[1]
-print labels
+def create_frequency_table(texts, labels):
+	freq_tbl = pd.DataFrame([])
+
+	for t, lbl in zip(texts, labels):
+		vocab = set(t)
+
+		d = pd.Series({ v : t.count(v) for v in vocab})
+		d['*class*'] = lbl
+
+		freq_tbl = freq_tbl.append(d, ignore_index=True)
+
+	freq_tbl = freq_tbl.fillna(0)
+
+	return freq_tbl
+
+del labels[0]
+del emails[0] #TODO
+
+def prob_of_classes(labels):
+	probs = {}
+
+	classes = set(labels)
+
+	for c in classes:
+		all_c = [x for x in labels if x == c]
+
+		prob_class = float(len(all_c)) / float(len(labels))
+
+		probs[c] = prob_class
+
+	return probs
+
+freq_tbl = create_frequency_table(emails, labels)
+
+class_probs = prob_of_classes(labels)
+
+
+
+
+
+
+
 
 
 
